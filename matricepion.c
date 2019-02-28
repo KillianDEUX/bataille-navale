bf9de9a9076c8ab52835d02b1725cfed093f7ece
corentinderouet <48018504+corentinderouet@users.noreply.github.com>
2019-02-28 12:02:29 +0100

    Update matricepion.c

--- matricepion.c
+++ matricepion.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define N 10
 
 typedef enum couleur { aucune, blanc, rouge } couleur_t;
 
@@ -10,27 +9,30 @@ typedef struct pion {
 		couleur_t c;	
 } pion_t;
 
-int init_matrice_adv (int taille){
+int creer_matrice_adv (int taille){
 	
-pion **ptr;
+	pion **ptr;
 
-ptr = malloc(taille * sizeof(*ptr)); 
-if(ptr == NULL){ 
-	return 1;
-} 
- ptr[0]= malloc(taille * taille * sizeof(*pts));
-     if(ptr[0]== NULL){
-	return 1;
-     }
-	
+	ptr = malloc(taille * sizeof(*ptr)); 
+	if(ptr == NULL){ 
+		return 1;
+	} 
+	 ptr[0]= malloc(taille * taille * sizeof(**ptr));
+	     if(ptr[0]== NULL){
+		return 1;
+	     }
+
+	}
+	for(int i=1; i<taille ; i++){
+		ptr[i]== ptr[i-1]+taille;
+	}
+
 }
-for(int i=1; i<taille ; i++){
-	ptr[i]== ptr[i-1]+taille;
-}
-	
-for(int i=0; i<taille ; i++)
-        for(int j=0; j<taille ; j++){
-            (ptr+i*taille+j).c= aucune;
+void init_matrice_adv (int taille){
+
+	for(int i=0; i<taille ; i++)
+       		for(int j=0; j<taille ; j++){
+           		(ptr[0]+i*taille+j).c= aucune;
 	}
 }
 
@@ -39,9 +41,9 @@ void ajout_pion_matrice( int taille, int abs, int ord ){
 	if( etat_tir(taille, abs, ord)==0){            // Si le tir tombe dans l'eau
 		 (ptr+abs*taille+ord).c= blanc ;     // Placer un pion blanc sur la matrice
 	}else if( etat_tir(taille, abs, ord)==1){      // Si le tir touche une cible
-		(ptr+abs*taille+ord).c= "rouge" ;      // Placer un pion rouge sur la matrice
+		(ptr+abs*taille+ord).c= rouge ;      // Placer un pion rouge sur la matrice
 	}else{					       // Si le tir coule une cible
-		(ptr+abs*taille+ord).c= "rouge" ;      // Placer un pion rouge sur la matrice
+		(ptr+abs*taille+ord).c= rouge ;      // Placer un pion rouge sur la matrice
 		eauautourcoule(taille, abs, ord);      // Placer des pion blancs tout autour sur la matrice
 	}
 	
@@ -58,31 +60,31 @@ void eauautourcoule( int taille, int abs, int ord ){
 	int ordtemp=ord+1;
 	int abstemp=abs;
 	if ( danslagrille(taille, abstemp, ordtemp)== 0){
-		if( (ptr+abstemp*taille+(ordtemp)).c= "aucune"){
-			(ptr+abstemp*taille+(ordtemp)).c= "blanc";
-		}else if ((ptr+abstemp*taille+(ordtemp)).c= "rouge"){
-			while((danslagrille(taille, abstemp, ordtemp)== 0) && ((ptr+abstemp*taille+ordtemp).c= "rouge")){
+		if( (ptr+abstemp*taille+(ordtemp)).c= aucune){
+			(ptr+abstemp*taille+(ordtemp)).c= blanc;
+		}else if ((ptr+abstemp*taille+(ordtemp)).c= rouge){
+			while((danslagrille(taille, abstemp, ordtemp)== 0) && ((ptr+abstemp*taille+ordtemp).c= rouge)){
 				abstemp=abs+1;
 				if(danslagrille(taille, abstemp, ordtemp)== 0){
-					(ptr+abstemp*taille+(ordtemp)).c= "blanc";
+					(ptr+abstemp*taille+(ordtemp)).c= blanc;
 				}
 				abstemp=abs-1;
 				if(danslagrille(taille, abstemp, ordtemp)== 0){
-					(ptr+abstemp*taille+(ordtemp)).c= "blanc";
+					(ptr+abstemp*taille+(ordtemp)).c= blanc;
 				}
 				ordtemp++;
 			}
 			abstemp=abs+1;
 			if(danslagrille(taille, abstemp, ordtemp)== 0){
-				(ptr+abstemp*taille+(ordtemp)).c= "blanc";
+				(ptr+abstemp*taille+(ordtemp)).c= blanc;
 			}
 			abstemp=abs-1;
 			if(danslagrille(taille, abstemp, ordtemp)== 0){
-				(ptr+abstemp*taille+(ordtemp)).c= "blanc";
+				(ptr+abstemp*taille+(ordtemp)).c= blanc;
 			}
 			abstemp=abs;
 			if(danslagrille(taille, abstemp, ordtemp)== 0){
-				(ptr+abstemp*taille+(ordtemp)).c= "blanc";
+				(ptr+abstemp*taille+(ordtemp)).c= blanc;
 			}
 		}
 	}
@@ -90,31 +92,31 @@ void eauautourcoule( int taille, int abs, int ord ){
 	int ordtemp=ord-1; 									// Changement 
 	int abstemp=abs;
 	if ( danslagrille(taille, abstemp, ordtemp)== 0){
-		if( (ptr+abstemp*taille+ordtemp).c= "aucune"){
-			(ptr+abstemp*taille+ordtemp).c= "blanc";
-		}else if ((ptr+abstemp*taille+ordtemp).c= "rouge"){
-			while((danslagrille(taille, abstemp, ordtemp)== 0) && ((ptr+abstemp*taille+ordtemp).c= "rouge")){
+		if( (ptr+abstemp*taille+ordtemp).c= aucune){
+			(ptr+abstemp*taille+ordtemp).c= blanc;
+		}else if ((ptr+abstemp*taille+ordtemp).c= rouge){
+			while((danslagrille(taille, abstemp, ordtemp)== 0) && ((ptr+abstemp*taille+ordtemp).c= rouge)){
 				abstemp=abs+1;
 				if(danslagrille(taille, abstemp, ordtemp)== 0){
-					(ptr+abstemp*taille+ordtemp).c= "blanc";
+					(ptr+abstemp*taille+ordtemp).c= blanc;
 				}
 				abstemp=abs-1;
 				if(danslagrille(taille, abstemp, ordtemp)== 0){
-					(ptr+abstemp*taille+ordtemp).c= "blanc";
+					(ptr+abstemp*taille+ordtemp).c= blanc;
 				}
 				ordtemp--;                   					// Changement 
 			}
 			abstemp=abs+1;
 			if(danslagrille(taille, abstemp, ordtemp)== 0){
-				(ptr+abstemp*taille+ordtemp).c= "blanc";
+				(ptr+abstemp*taille+ordtemp).c= blanc;
 			}
 			abstemp=abs-1;
 			if(danslagrille(taille, abstemp, ordtemp)== 0){
-				(ptr+abstemp*taille+ordtemp).c= "blanc";
+				(ptr+abstemp*taille+ordtemp).c= blanc;
 			}
 			abstemp=abs;
 			if(danslagrille(taille, abstemp, ordtemp)== 0){
-				(ptr+abstemp*taille+ordtemp).c= "blanc";
+				(ptr+abstemp*taille+ordtemp).c= blanc;
 			}
 		}
 	}
@@ -122,31 +124,31 @@ void eauautourcoule( int taille, int abs, int ord ){
 	int ordtemp=ord;                   				                        // Changement 			
 	int abstemp=abs+1;                  					                // Changement 
 	if ( danslagrille(taille, abstemp, ordtemp)== 0){
-		if( (ptr+abstemp*taille+ordtemp).c= "aucune"){
-			(ptr+abstemp*taille+ordtemp).c= "blanc";
-		}else if ((ptr+abstemp*taille+ordtemp).c= "rouge"){
-			while((danslagrille(taille, abstemp, ordtemp)== 0) && ((ptr+abstemp*taille+ordtemp).c= "rouge")){
+		if( (ptr+abstemp*taille+ordtemp).c= aucune){
+			(ptr+abstemp*taille+ordtemp).c= blanc;
+		}else if ((ptr+abstemp*taille+ordtemp).c= rouge){
+			while((danslagrille(taille, abstemp, ordtemp)== 0) && ((ptr+abstemp*taille+ordtemp).c= rouge)){
 				ordtemp=ord+1;							// Changement
 				if(danslagrille(taille, abstemp, ordtemp)== 0){
-					(ptr+abstemp*taille+(ordtemp)).c= "blanc";
+					(ptr+abstemp*taille+(ordtemp)).c= blanc;
 				}
 				ordtemp=ord-1;							// Changement
 				if(danslagrille(taille, abstemp, ordtemp)== 0){
-					(ptr+abstemp*taille+ordtemp).c= "blanc";
+					(ptr+abstemp*taille+ordtemp).c= blanc;
 				}
 				abstemp++;							// Changement                   					
 			}
 			ordtemp=ord+1;								// Changement
 			if(danslagrille(taille, abstemp, ordtemp)== 0){
-				(ptr+abstemp*taille+ordtemp).c= "blanc";
+				(ptr+abstemp*taille+ordtemp).c= blanc;
 			}
 			ordtemp=ord-1;								// Changement
 			if(danslagrille(taille, abstemp, ordtemp)== 0){
-				(ptr+abstemp*taille+ordtemp).c= "blanc";
+				(ptr+abstemp*taille+ordtemp).c= blanc;
 			}
 			ordtemp=ord;								// Changement
 			if(danslagrille(taille, abstemp, ordtemp)== 0){
-				(ptr+abstemp*taille+ordtemp).c= "blanc";
+				(ptr+abstemp*taille+ordtemp).c= blanc;
 			}
 		}
 	}
@@ -154,31 +156,31 @@ void eauautourcoule( int taille, int abs, int ord ){
 	int ordtemp=ord;                   				                        		
 	int abstemp=abs-1;                  					                // Changement 
 	if ( danslagrille(taille, abstemp, ordtemp)== 0){
-		if( (ptr+abstemp*taille+ordtemp).c= "aucune"){
-			(ptr+abstemp*taille+ordtemp).c= "blanc";
-		}else if ((ptr+abstemp*taille+ordtemp).c= "rouge"){
-			while((danslagrille(taille, abstemp, ordtemp)== 0) && ((ptr+abstemp*taille+ordtemp).c= "rouge")){
+		if( (ptr+abstemp*taille+ordtemp).c= aucune){
+			(ptr+abstemp*taille+ordtemp).c= blanc;
+		}else if ((ptr+abstemp*taille+ordtemp).c= rouge){
+			while((danslagrille(taille, abstemp, ordtemp)== 0) && ((ptr+abstemp*taille+ordtemp).c= rouge)){
 				ordtemp=ord+1;							
 				if(danslagrille(taille, abstemp, ordtemp)== 0){
-					(ptr+abstemp*taille+(ordtemp)).c= "blanc";
+					(ptr+abstemp*taille+(ordtemp)).c= blanc;
 				}
 				ordtemp=ord-1;							
 				if(danslagrille(taille, abstemp, ordtemp)== 0){
-					(ptr+abstemp*taille+ordtemp).c= "blanc";
+					(ptr+abstemp*taille+ordtemp).c= blanc;
 				}
 				abstemp--;							// Changement                   					
 			}
 			ordtemp=ord+1;								
 			if(danslagrille(taille, abstemp, ordtemp)== 0){
-				(ptr+abstemp*taille+ordtemp).c= "blanc";
+				(ptr+abstemp*taille+ordtemp).c= blanc;
 			}
 			ordtemp=ord-1;
 			if(danslagrille(taille, abstemp, ordtemp)== 0){
-				(ptr+abstemp*taille+ordtemp).c= "blanc";
+				(ptr+abstemp*taille+ordtemp).c= blanc;
 			}
 			ordtemp=ord;
 			if(danslagrille(taille, abstemp, ordtemp)== 0){
-				(ptr+abstemp*taille+ordtemp).c= "blanc";
+				(ptr+abstemp*taille+ordtemp).c= blanc;
 			}
 		}
 	}
