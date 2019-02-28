42bd3ddd7185d61847a38016b99c06270eff6ee4
KillianDEUX <48018475+KillianDEUX@users.noreply.github.com>
2019-02-28 10:28:30 +0100

    Update matricebateau.c

--- matricebateau.c
+++ matricebateau.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h
+#include "liste.h"
 
-typedef struct typebateau{
-       int taille;
+
+void choixbateau(int N, int M){
+       init_liste();
+       char fich[20] = 'bateau.txt';
+       FILE * fic1;
+       int nbbat=0;
+       int nbmaxbat = (N*M)/2;
+       fic1= fopen(fich, "w");
+       while(nbbat<= 0 || nbbat > nbmaxbat){
+              printf("Combien de bâteau voulez-vous avoir ?(différent de 0 et inférieur à la moitié du nombre de case du terrain de jeu) : ");
+              scanf("%i", &nbbat);
+       }
+       fprintf(fic1,"%i", nbbat);
+       for(int i=0; i<nbbat;i++){
+              printf("Ecrire la taille du %i bateau(entre 1 et 5) : ",i);
+              if(i<=5 && i>=1){
+                     scanf("%i", &t);
+                     fprintf(fic1, "%i", t);
+              }
+       }
 }
-
+       
