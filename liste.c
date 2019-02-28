ed48c1b8f6e15a511b7d41ee90bd1ee44b1ce15c
KillianDEUX <48018475+KillianDEUX@users.noreply.github.com>
2019-02-28 09:47:00 +0100

    Update liste.c

--- liste.c
+++ liste.c
@@ -1,112 +1,96 @@
-/* Mise en oeuvre contigue d'une liste d'entiers */
-#define TAILLE_MAX 100
 #include<stdio.h>
+#include<stdlib.h>
+typedef struct element{int valeur; 
+	struct element * pred; 
+	struct element * succ
+	}t_element;
+t_element * drapeau;
+t_element * ec;
 
-/* Declaration du tableau contenant les elements de liste */
-int liste[TAILLE_MAX];
-/* Declaration des indices de queue et d'element courant */
-int queue, ec;
-
-void init_liste(void){
-/* Initialise la liste a vide */
-
-	queue = -1;
-	ec = -1;
+void init_liste(){
+	drapeau= malloc(sizeof(t_element));
+	drapeau->pred= drapeau;
+	drapeau->succ=drapeau;
+	ec=drapeau;
 }
 
-int liste_vide(void){
-/* Rend vrai si la liste est vide, faux sinon */
-
-	return (queue== -1);
+int liste_vide(){
+	return (drapeau->succ == drapeau);
 }
 
-int hors_liste(void){
-/* Rend vrai si l'elt courant est hors de la liste, faux sinon */
-
-	return(ec < 0 || ec > queue);
+int hors_liste(){
+	return (ec==drapeau);
 }
 
-void en_tete(void){
-/* Positionne en tete de la liste */
-
-	if(!liste_vide()) 
-		ec = 0;
+void en_tete(){
+	if(!liste_vide()){
+		ec=drapeau->succ;
+	}
 }
 
-void en_queue(void){
-/* Positionne en queue de la liste */
-
-	if(!liste_vide()) 
-		ec = queue;
+void en_queue(){
+	if(!liste_vide()){
+		ec=drapeau->pred;
+	}
 }
 
-void precedent(void){
-/* Positionne sur l'elt precedent*/
-
-	if(!hors_liste()) 
-		ec--;
+void suivant(){
+	if(!hors_liste()){
+		ec=ec->succ;
+	}
 }
 
-void suivant(void){
-/* Positionne sur l'elt suivant*/
-
-	if(!hors_liste()) 
-		ec++;
+void precedent(){
+	if(!hors_liste()){
+		ec=ec->pred;
+	}		
 }
 
-void valeur_elt(int* v){
-/* Renvoie dans v la valeur de l'elt courant */
-
-	if(!hors_liste()) 
-		*v= liste[ec];
+void valeur_elt(int * v){
+	if(!hors_liste()){
+		*v=ec->valeur;
+	}
 }
 
-void modif_elt(int v){
-/* Affecte v a l'elt courant */
-
-	if(!hors_liste()) 
-		liste[ec]=v;
+void modif_elt(int * v){
+	if(!hors_liste()){	
+		ec->valeur= v;
+		(ec->pred)->succ=v;
+		(ec->succ)->pred=v;		
+	}
 }
 
-void oter_elt(void){
-/* Supprime l'elt courant et positionne sur le precedent */
-
-	int i;
-	if(!hors_liste())
-	{	
-		for(i=ec;i<queue;i++)
-			liste[i]=liste[i+1];
-		ec--; queue--;
+	
+void oter_elt(){
+	t_element * ancien;
+	if(!hors_liste()){
+		(ec->pred)->succ=ec->pred;
+		(ec->succ)->pred=ec->succ;
+		ancien=ec;
+		ec=ec->pred;
+		free(ancien);
 	}
 }
 
 void ajout_droit(int v){
-/* Ajoute v a droite de l'elt courant */
-
-	int i;
-	
-	if(liste_vide()||!hors_liste())
-	{	
-		for(i=queue;i>ec;i--)
-			liste[i+1]=liste[i];
-		liste[ec+1]=v;
-		ec++; queue++;
+	t_element * nouveau;
+	if(!hors_liste()){
+		nouveau=malloc(sizeof(t_element));
+		nouveau->pred=ec->valeur;
+		nouveau->succ=ec->succ;
+		ec->succ=v;
+		(nouveau->succ)->pred=v;
 	}
 }
+
 
 void ajout_gauche(int v){
-/* Ajoute v a gauche de l'elt courant */
-
-	int i;
-	
-	if(liste_vide())
-		ec++;
-
-	if(liste_vide()||!hors_liste()){	
-		for(i=queue;i>=ec;i--)
-			liste[i+1]=liste[i];
-		liste[ec]=v;
-		queue++;
+	t_element * nouveau;
+	if(!hors_liste()){
+		nouveau=malloc(sizeof(t_element));
+		nouveau->pred=ec->pred;
+		nouveau->succ=ec->valeur;
+		ec->pred=v;
+		(nouveau->pred)->succ=v;
 	}
 }
-
