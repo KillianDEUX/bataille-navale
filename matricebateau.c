999e71aa12d4112977ee5f974ae0b2884f770622
KillianDEUX <48018475+KillianDEUX@users.noreply.github.com>
2019-03-05 14:09:46 +0100

    Update matricebateau.c

--- matricebateau.c
+++ matricebateau.c
@@ -2,28 +2,6 @@
 #include <stdlib.h
 #include "liste.h"
 
-
-void choixbateau(int N, int M){
-       init_liste();
-       char fich[20] = 'bateau.txt';
-       FILE * fic1;
-       int nbbat=0;
-       int nbmaxbat = (0,06 * (M*N)) - (0,3 * N) +2;
-       fic1= fopen(fich, "w");
-       while(nbbat<= 0 || nbbat > nbmaxbat){
-              printf("Combien de bâteau voulez-vous avoir ?(différent de 0 et inférieur à la moitié du nombre de case du terrain de jeu) : ");
-              scanf("%i", &nbbat);
-       }
-       fprintf(fic1,"%i", nbbat);
-       for(int i=0; i<nbbat;i++){
-              printf("Ecrire la taille du %i bateau(entre 1 et 5) : ",i);
-              if(i<=5 && i>=1){
-                     scanf("%i", &t);
-                     fprintf(fic1, "%i", t);
-              }
-       }
-}
-       
 typedef struct bateau {
 		int b;	
 } bateau_t;
@@ -60,6 +38,32 @@ int init_matrice_bat (int taille, bateau_t bat[]){
 void vider_matrice(int taille){
 	init_matrice_adv (taille);
 }
+
+
+void choixbateau(int taille){
+       char fich[20] = 'bateau.txt';
+       FILE * fic1;
+       int nbbat=0;
+       int nbmaxbat = (0,06 * (M*N)) - (0,3 * N) +2;
+       fic1= fopen(fich, "w");
+       while(nbbat<= 0 || nbbat > nbmaxbat){
+              printf("Combien de bâteau voulez-vous avoir ?(différent de 0 et inférieur à la moitié du nombre de case du terrain de jeu) : ");
+              scanf("%i", &nbbat);
+       }
+       fprintf(fic1,"%i", nbbat);
+       for(int i=0; i<nbbat;i++){
+              printf("Ecrire la taille du %i bateau(entre 1 et 5) : ",i);
+              if(i<=5 && i>=1){
+                     scanf("%i", &t);
+                     fprintf(fic1, "%i", t);
+              }
+       }
+}
+       
+
+
+
+
 
 // Voir avec la SDL. Ici version terminal
 void afficher_matrice_pion( int taille){
