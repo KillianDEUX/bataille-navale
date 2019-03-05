aff96d1e5d7c8410d7dbc38d272e9b384359a3c3
KillianDEUX <48018475+KillianDEUX@users.noreply.github.com>
2019-03-05 18:16:02 +0100

    Update matricebateau.c

--- matricebateau.c
+++ matricebateau.c
@@ -2,21 +2,24 @@
 #include <stdlib.h>
 #include "liste.h"
 
-typedef enum type_bat {mine ,torpilleur, sousmarin,  croiseur, porte-avion} type_t;
-typedef enum dir_bat {vertical, horizontal} dir_t;
+typedef enum type_bat {aucun, mine ,torpilleur, sousmarin,  croiseur, porte-avion}type_t;
+typedef enum dir_bat {aucun, vertical, horizontal}dir_t;
+typedef enum etat_bat {flotte, coule}etat_t;
 
 typedef struct bateau {
-	type_t b;
+	type_t type;
 	int coordx;
 	int coordy;
 	int taille;
-	dir_t bat;
-} bateau_t;
+	dir_t dir;
+	etat_t etat;
+}bateau_t;
 
 
 void choixbateau(int taille){
        init_liste();
        int nbbat=0;
+       bateau_t nouveau;
        int nbmaxbat = (0,06 * (taille*taille)) - (0,3 * taille) +2;
        while(nbbat<= 0 || nbbat > nbmaxbat){
               printf("Combien de bâteau voulez-vous avoir ?(différent de 0 et inférieur à la moitié du nombre de case du terrain de jeu) : ");
@@ -27,36 +30,64 @@ void choixbateau(int taille){
               printf("Ecrire la taille du %i bateau(entre 1 et 5) : ",i);
               if(i<=5 && i>=1){
                      scanf("%i", &t);
-                     
+                     en_tete();
+                     nouveau->taille = t;
+                     nouveau->coordx = -1;
+                     nouveau->coordy = -1;
+                     nouveau->dir = aucun;
+                     nouveau->type = aucun;
+                     nouveau->etat = flotte;
+                     ajout_droit(nouveau);
               }
        }
 }
        
-
-
-
-
-
-// Voir avec la SDL. Ici version terminal
-void afficher_matrice( int taille){
-	
-
-
-	/*for(int i=0; i<taille ; i++)
-       		for(int j=0; j<taille ; j++){
-           		if((bat[0]+i*taille+j).p == 0){
-				printf(". ");
-			}
-			else{
-				printf(" [] ");
-			}
-		}
-		printf("\n");
-	}*/
+position_bateau_vertical(bateau_t bateau){
+	return (bateau->coordy + bateau->taille);
+}
+       
+position_bateau_horizontal(bateau_t bateau){
+	return (bateau->coordx + bateau->taille); 
 }
 
+// Voir avec la SDL. Ici version terminal
+void afficher_matrice_bateau(int taille){
+	int coordx, coordy;
+	bateau_t * actuel;
+	for(int i=0; i<taille ; i++){
+       	for(int j=0; j<taille ; j++){
+       		entete();
+       		while(!hors_liste()){
+       			valeur_elt(*actuel);
+       			if(i==actuel->coordx && j==actuel->coordy){
+       				if(actuel->dir == vertical){
+       					position_bateau_vertical(actuel);
+       					for(int k=j; k< (position_bateau_vertical(actuel)); k++){
+       						printf("[.]");
+       					}
+       				}
+       				if(actuel->dir == horizontal){
+       					position_bateau_horizontal(actuel);
+       					for(int k=j; k< (position_bateau_horizontal(actuel)); k++){
+       						printf("[.]");
+       					}
+       				}
+       			}
+       			else{
+       				printf("[]");
+       			}
+       			suivant();
+       		}
+       	}
+    }
+}
+
+
 void ( int taille, int abs, int ord, bateau_t bat[] ){
-	// Lorsque le bateau est dirigé vers le nord
+	
+	
+	
+	/*// Lorsque le bateau est dirigé vers le nord
 	int ordtemp=ord+1;
 	int abstemp=abs;
 	if ( danslagrille(taille, abstemp, ordtemp)== 0){
@@ -171,29 +202,12 @@ void ( int taille, int abs, int ord, bateau_t bat[] ){
 				(ptr[0]+abstemp*taille+ordtemp).c= blanc;
 			}
 		}
-	}
+	}*/
 }
 
 void placerbateau(bateau_t bat[], int taille, int n_bateau, char dir, case_t emp){
-	FILE *fic;
-	int entier, nb_bateau;
-	int nb_case_bateau = 0; 
-	int pos;
-	fic = fopen("bateau.txt", "r");
-	for(int i=; !feof(fic); i++){
-		fscanf(fic, "%i", &entier);
-		if(i == 0){
-			nb_bateau = entier;
-		}
-		else if(i == n_bateau){
-			 nb_case_bateau = entier;
-		}
-		if(nb_bateau < n_bateau){
-			printf("Ce bateau n'existe pas");
-			exit 0;	
-		}
-	}
-	pos = case_coordtotab(taille, emp);
+
+}
 	
 	
 	
