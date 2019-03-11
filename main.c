816c2c6220dde295c58a17296c038cc67e9a54fb
corentinderouet <48018504+corentinderouet@users.noreply.github.com>
2019-03-11 13:13:52 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+  int nb_joueurs =  // voir sur le serveur
+   switch( nb_joueurs){
+     case 5 :  t_liste joueur5;
+     case 4 :  t_liste joueur4;
+     case 3 :  t_liste joueur3;
+     case 2 :  t_liste joueur2;
+     case 1 :  t_liste joueur1; break;
+     default : printf("Nombre de joueurs incorrect !");
+   }
+   if(nb_joueurs==1){
+     t_liste ia;
+     
+     // TOURS DE JEU AVEC L'IA
+   }else{
+     // TOURS DE JEU EN RESEAU
+   }
+}
