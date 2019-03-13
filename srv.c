f616466fe3e9288e3c449109b37acabcb807676b
KillianDEUX <48018475+KillianDEUX@users.noreply.github.com>
2019-03-13 14:12:28 +0100

    Add files via upload

--- srv.c
+++ srv.c
@@ -6,20 +6,18 @@
 #define PORT 32000
 
 
-int main(){
+void serveur(){
 
 	struct sockaddr_in server_addr, client_addr;    
 	socklen_t client_addr_len;
 	int port_no=PORT;
 	int i;
-	int nb_cli;
+	int nb_cli=0;
 	int client_fd[nb_cli];
 	int choix_c_atk=1;
-	int question;
 	int * server_fd;
 	int choix_j_atk;
 	int nb_joueur;
-	int nb_joueur_atk;
 	server_fd = malloc(sizeof(int));
 	int tour_atk;
 
@@ -33,8 +31,7 @@ int main(){
 	do{			   
 		printf("\n Bonjour ! A combien voulez-vous jouer aujoud'hui ? ");
 			    scanf("%i", &nb_cli);
-			   }
-	while (nb_cli<2 || nb_cli>10);
+	}while (nb_cli<2 || nb_cli>10);
 
 
 	// Definition du socket
@@ -116,78 +113,4 @@ int main(){
 
 
 	}
-	// }
 }
-	
-	/**
-
-	while(1==1){
-		do{			   
-			  printf("\nChoisissez le joueur à attaquer : ");
-			  scanf("%i", &choix_j_atk);
-			   }
-		while (choix_j_atk<2 || choix_j_atk>nb_cli+1);
-			
-			printf("Quelle case voulez-vous attaquer ? ");
-			scanf("%i", &question);
-			for(i=0; i<nb_cli; i++){
-			    	send(client_fd[i], &nb_joueur_atk, sizeof(nb_joueur_atk), 0);
-				send(client_fd[i], &choix_j_atk, sizeof(choix_j_atk), 0);
-				send(client_fd[i], &question, sizeof(question), 0);
-						
-			}
-
-				
-
-		}
-		
-	}**/
-
-
-
-/**
-void tcp_connection_server(int * server_fd, int * client_fd) {
-    struct sockaddr_in server_addr, client_addr; 
-    socklen_t client_addr_len;
-    int port_no=PORT;
-
-    int conn; //This is the connection file descriptor that will be used to distinguish client connections.
-	char message[100]; //This array will store the messages that are sent by the server
-
-	client_addr_len = sizeof(client_addr);
-
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(port_no);
-
-    //assigns the address specified by serv to the socket
-    bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-
-    listen(server_fd,2); //Listen for client connections. Maximum 5 connections will be permitted
-
-    printf("\n -- EN ATTENTE --\n");
-
-	*client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-
-	printf("\n -- CONNECTE --\n");
-
-
-
-
-			recv(client_fd[nb_cli], &choix_c_atk, sizeof(choix_c_atk), 0);
-
-
-			while (choix_c_atk!=0){
-
-				printf(" AIE ! - j'ai mal à la case : \n %i \n \n", choix_c_atk);
-				choix_c_atk=0;
-				printf("A mon tour, quelle case voulez-vous attaquer ? ");
-				scanf("%i", &question);
-				send(client_fd[nb_cli], &question, sizeof(question), 0);
-				printf("\n");
-			}
-		    
- 
-}
-**/
