1680b56b3c630cb795a2db161fb05ed700fe81a0
KillianDEUX <48018475+KillianDEUX@users.noreply.github.com>
2019-03-18 14:32:26 +0100

    Add files via upload

--- listebateau.h
+++ listebateau.h
@@ -6,15 +6,16 @@
 #include "liste.h"
 #include "bateau.h"
 
+int etat_tir( int taille, case_t cell, t_liste joueur);
+int veriftouche( int taille, case_t cell, t_liste joueur);
+int bateaux_coules( t_liste joueur, int nb_bat);
 void type_bateau(t_liste);
-void choixbateau(t_liste,int);
 void appliquer_bateau(t_liste,t_liste);
-int fin_bateau_vertical(t_liste *, bateau_t *);
-int fin_bateau_horizontal(t_liste *, bateau_t *);
-void afficher_matrice_bateau(t_liste, int);
+void affichage_flotte(t_liste joueur, int taille)
 void cases_prises(t_liste, case_t *, int);
 int placement_bateau(t_liste, bateau_t *, dir_t, case_t, int);
 void placer_bateau(t_liste, int);
+void choixbateau(t_liste,int);
 int bateau_plus_petit(t_liste);
 
 #endif
