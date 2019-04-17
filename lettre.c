a0ef9f92121e6ddbd61c1e68a07e91e3a5c6c7b8
KillianDEUX <48018475+KillianDEUX@users.noreply.github.com>
2019-04-17 16:49:12 +0200

    Commentaire fichier lettre

--- lettre.c
+++ lettre.c
@@ -1,11 +1,13 @@
 #include "lettre.h"
 
+//change le caractère fourni en paramètre en nombre
 int lettre_to_nombre(char c){
 	int i;
 	i = c - 65;
 	return i;
 }
 
+//Change le nombre fourni en paramètre en caractère
 char nombre_to_lettre(int i){
 	char c;
 	c = i + 65;
--- lettre.h
+++ lettre.h
@@ -1,7 +1,10 @@
 #ifndef __DIRECTION_H__
 #define __DIRECTION_H__
 
+//change le caractère fourni en paramètre en nombre
 int lettre_to_nombre(char);
+
+//Change le nombre fourni en paramètre en caractère
 char nombre_to_lettre(int);
 
 #endif
